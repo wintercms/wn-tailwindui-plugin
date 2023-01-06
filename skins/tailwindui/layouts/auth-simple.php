55b246b28d8b503821c7d077aecb7b8b92b8e159
Ondrej Brinkel <info@anzui.dev>
2023-01-06 10:27:29 +0100

    makes auth screens dark mode compatible

--- skins/tailwindui/layouts/auth-simple.php
+++ skins/tailwindui/layouts/auth-simple.php
@@ -12,7 +12,7 @@
             </div>
 
             <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md sm:max-w-sm">
-              <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+              <div class="bg-white dark:bg-gray-800 py-8 px-4 shadow sm:rounded-lg sm:px-10">
                     <?= Block::placeholder('body') ?>
               </div>
             </div>
