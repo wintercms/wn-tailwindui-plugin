5978a7428c853b09946489caf68a96893b843396
Luke Towers <luke@luketowers.ca>
2023-08-07 22:47:42 -0600

    Remove embedded style tag
    Fixed in https://github.com/wintercms/winter/commit/e8a1285ff3f418d53537cb3b46e18c9a3b037f4f

--- skins/tailwindui/layouts/partials/notices/_impersonation.php
+++ skins/tailwindui/layouts/partials/notices/_impersonation.php
@@ -1,14 +1,4 @@
 <?php if (\BackendAuth::isImpersonator()) : ?>
-    <style>
-        /**
-            - @TODO:
-            - Luke fix this, needs to go into the winter.css source LESS file and be recompiled
-            - https://github.com/wintercms/winter/commit/857d42101e1955cd0cd8933360796bd4a802a5c0
-            */
-        .global-notice{position:sticky;top:0;display:flex;align-items:center;flex-wrap:wrap;gap:0.5em;justify-content:space-between;z-index:10500;background:#ab2a1c;color:#FFF;padding:0.5em 0.75em}
-        .global-notice .notice-icon{font-size:1.5em;vertical-align:bottom;display:inline-block;margin-right:.25em}
-        .global-notice .notice-text{display:inline-block;vertical-align:middle}
-    </style>
     <div class="global-notice">
         <div class="notice-content">
             <span class="notice-text">
